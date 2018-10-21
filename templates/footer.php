8fb283a1ed538135b0b85e156f16d06809289e4e
Luis Saavedra <luis@saavedral.me>
2018-10-21 22:37:45 +1100

    Added footer

---
+++ templates/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+  <div class="columns">
+    <div class="column is-6 twitter-feed">
+      <h4>Twitter Feed</h4>
+      <div>
+        <p>Blockchain 101! Our first Blockchain Education Event, partnering with Merklize. Book it now, If you would like to be trained by one of the best experts in the market. <a>Find out More → </a></p>
+      </div>
+      <div>
+        <p>Blockchain 101! Our first Blockchain Education Event, partnering with Merklize. Book it now, If you would like to be trained by one of the best experts in the market. <a>Find out More → </a></p>
+      </div>
+    </div>
+    <div class="column is-2">
+      <h4>Navigation</h4>
+      <p>About us</p>
+      <p>Roadmap</p>
+      <p>Partners</p>
+      <p>Insights</p>
+      <p>Contact us</p>
+    </div>
+    <div class="column is-2">
+    <h4>Products</h4>
+
+    <p>Blockchain advisory</p>
+    <p>Blockchain</p>
+    <p>Accountancy</p>
+    <p>Blockchain wallet</p>
+    </div>
+    <div class="column is-2">
+    <h4>Social media</h4>
+    <p>LinkedIn</p>
+    <p>Facebook</p>
+    <p>Twitter</p>
+    <p>Reddit</p>
+    <p>Medium</p>
+    <p>SteemIT</p>
+    <p>Telegram</p>
+    </div>
+  </div>
+</footer>
