e606bc7d62aafdde4cba83fdbcc75dced92e8260
Kellson <kellsonmar@hotmail.com>
2018-11-29 10:35:51 +1100

    Update footer links and style

--- templates/footer.php
+++ templates/footer.php
@@ -12,11 +12,11 @@
       </div>
       <div class="column is-2">
         <h4>Navigation</h4>
-        <p>About us</p>
-        <p>Roadmap</p>
-        <p>Partners</p>
-        <p>Insights</p>
-        <p>Contact us</p>
+        <p><a href="about-us.php" class="static-link">About us</a></p>
+        <p><a href="roadmap.php" class="static-link">Roadmap</a></p>
+        <p><a href="partners.php" class="static-link">Partners</a></p>
+        <p><a href="insights.php" class="static-link">Insights</a></p>
+        <p><a href="contact-us" class="static-link">Contact us</a></p>
       </div>
       <div class="column is-2">
       <h4>Products</h4>
@@ -28,13 +28,13 @@
       </div>
       <div class="column is-2">
       <h4>Social media</h4>
-      <p>LinkedIn</p>
-      <p>Facebook</p>
-      <p>Twitter</p>
-      <p>Reddit</p>
-      <p>Medium</p>
-      <p>SteemIT</p>
-      <p>Telegram</p>
+      <p><a href="https://www.linkedin.com/company/ledgerium" class="static-link">LinkedIn</a></p>
+      <p><a href="https://www.facebook.com/Ledgerium" class="static-link">Facebook</a></p>
+      <p><a href="https://t.me/LedgeriumOfficial" class="static-link">Telegram</a></p>
+      <p><a href="https://twitter.com/ledgerium" class="static-link">Twitter</a></p>
+      <p><a href="https://weixin.qq.com/r/GykgOAHEi1z0rUVb93wJ" class="static-link">WeChat</a></p>
+      <p><a href="https://medium.com/@ledgerium.m" class="static-link">Medium</a></p>
+      <p><a href="https://www.reddit.com/user/Ledgerium" class="static-link">Reddit</a></p>
       </div>
     </div>
   </div>
